a4c13a4821074d7d21991d88af97ac1dd211d0e9
Tyler Kindy <tkindy@hubspot.com>
2018-02-18 00:24:59 +0000

    Add 'add project' action

---
+++ docs/js/add-to-project.js
@@ -0,0 +1,28 @@
+const t = window.TrelloPowerUp.iframe();
+
+window.project.addEventListener('submit', (event) => {
+  // Stop the browser trying to submit the form itself.
+  event.preventDefault();
+  
+  return t.set('card', 'shared', 'project', window.projectName.value)
+    .then(() => {
+      t.closePopup();
+    });
+});
+
+t.render(() => {
+  const select = window.projectName;
+  
+  return t.get('board', 'shared', 'projects')
+    .then(projects => {
+      for (let project of projects) {
+        const option = document.createElement('option');
+        option.text = project;
+        
+        select.add(option);
+      }
+    })
+    .then(() => {
+      t.sizeTo('#project').done();
+    });
+});
